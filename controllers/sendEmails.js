0da977c6cab14bfe006b93cb7604a186f0fcf59c
Johnny B <typngeek@aim.com>
2018-08-07 15:22:54 -0400

    Create Email Sending Module

---
+++ controllers/sendEmails.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express.Router();
+const User = require('../models/user');
+const ResetReq = require('../models/resetreqs');
+const fs = require('fs');
+const path = require('path');
+
+// Require Mailgun
+var mailgun = require('mailgun-js')({
+    apiKey: process.env.mailgunKey,
+    domain: process.env.mailgunDomain
+});
+
+/**
+ * Function to send an email to a receipient with an optional button.
+ * Returns undefined
+ * @param {String} emailAddress of email recpient
+ * @param {Object} body object with parameters subject, title, preheader, superheader, header, paragraph, buttonText, buttonLink
+ */
+function sendEmail(emailAddress, body) {
+    const pug = require('pug');
+
+    var emailbody = pug.renderFile('./views/emailtemplate.pug', {
+        title: body.title,
+        preheader: body.preheader,
+        superheader: body.superheader,
+        header: body.header,
+        paragraph: body.paragraph,
+        buttonText: body.buttonText,
+        buttonLink: body.buttonLink
+    });
+    console.log(emailbody);
+    var data = {
+        from: 'ZBudgeT <support@johnnybui.com>',
+        to: emailAddress,
+        subject: body.subject,
+        html: emailbody
+    };
+    mailgun.messages().send(data, function (error, body) {
+        if (error) console.log(error);
+        console.log(body);
+    });
+}
+
+module.exports = sendEmail
